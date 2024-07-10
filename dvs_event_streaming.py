edbbb39eec52636283a2c640d5050fb79f1270ae
Andrea Pignata <62184044+andreapignaz@users.noreply.github.com>
2024-07-10 17:43:43 +0200

    Add files via upload

---
+++ dvs_event_streaming.py
@@ -0,0 +1,59 @@
+import dv_processing as dv
+import argparse
+from socket import *
+
+# Camera name for DAVIS346
+# DAVIS346_00000499
+# or launch dv-list-devices
+
+parser = argparse.ArgumentParser(description='Record data from a single iniVation camera to a file.')
+parser.add_argument("-c",
+                    "--camera_name",
+                    dest='camera_name',
+                    default="",
+                    type=str,
+                    help="Camera name (e.g. DVXplorer_DXA00093). The application will open any supported camera "
+                    "if no camera name is provided.")
+parser.add_argument("-s",
+                    "--server_address",
+                    dest='server_address',
+                    default="localhost",
+                    type=str,
+                    help="IP Address of the destination server. If nothing is set, localhost will be used.")
+parser.add_argument("-p",
+                    "--server_port",
+                    dest='server_port',
+                    default=12000,
+                    type=str,
+                    help="IP Address of the destination server. If nothing is set, 12000 will be used.")           
+args = parser.parse_args()
+
+print("Configure camera")
+# Open any camera that is discovered in the system
+camera = dv.io.CameraCapture(args.camera_name)
+
+# Check whether frames are available
+if not(camera.isEventStreamAvailable()):
+    print("Event streaming not available for this camera.")
+    quit()
+
+print("Configure UDP")
+clientSocket = socket(AF_INET, SOCK_DGRAM)
+clientSocket.settimeout(1)
+addr = (args.server_address, args.server_port)
+
+print("Start streaming")
+
+try: 
+    while camera.isConnected():
+        # Get Events
+        events = camera.getNextEventBatch()
+        # Process  Events
+        if events is not None:
+            for ev in events:
+                message = f"({ev.timestamp()}, {ev.x()}, {ev.y()}, {ev.polarity()})"
+                clientSocket.sendto(message.encode(), addr)
+
+except KeyboardInterrupt:
+    print("Ending streaming")
+    pass
