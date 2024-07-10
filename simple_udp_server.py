3f07e6e14941dec9faeab1edbb10a82d4154f410
Andrea Pignata <62184044+andreapignaz@users.noreply.github.com>
2024-07-10 17:47:22 +0200

    Add files via upload

---
+++ simple_udp_server.py
@@ -0,0 +1,15 @@
+from socket import *
+
+# Create a UDP socket
+# Notice the use of SOCK_DGRAM for UDP packets
+serverSocket = socket(AF_INET, SOCK_DGRAM)
+
+# Assign IP address and port number to socket
+serverSocket.bind(('130.192.163.100', 12000))
+
+while True:
+    # Receive the client packet along with the address it is coming from
+    message, address = serverSocket.recvfrom(1024)
+    # Capitalize the message from the client
+    message = message.decode().upper()
+    print(f"Received {message} from {address}")
